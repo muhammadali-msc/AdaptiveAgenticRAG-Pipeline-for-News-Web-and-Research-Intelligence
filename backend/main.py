40a13394f367b0211af0f0c0aa697e912740d2b7
Muhammad Ali <muhammadalimsc21@gmail.com>
2025-07-28 21:22:03 +0500

    Added the docker functionality, test locally by building the container and image, and Deploy it on AWS server

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],
+    allow_origins=["http://localhost:3000", "*","https://54.210.34.140:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
